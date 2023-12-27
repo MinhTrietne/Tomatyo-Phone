ccf6b2be3989ca35a32730f92880e6c700646e4a
MinhTrietne <leminhtriet2691@gmail.com>
2023-12-27 14:33:44 +0700

    lates files

---
+++ show1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title>Image Upload</title> 
+<link rel="stylesheet" href="style.css"> 
+</head> 
+<body> 
+<div id="content"> 
+<?php require "xuly.php"?>
+</form> 
+</div> 
+</body> 
+</html>
